ba54fdb1b58d1fa608a247f5bb0d1cf89870b53e
QusaiHroub <qusaihroub.r@gmail.com>
2020-04-14 22:43:10 +0300

    Add getRandomParticle

--- src/utilities.hpp
+++ src/utilities.hpp
@@ -3,8 +3,12 @@
 
 #include <vector>
 #include <algorithm>
+#include <random>
 
 #include "particle.hpp"
+#include "maplevel99plus.hpp"
+
+const unsigned long long PRIME = 261798184036870849ll;
 
 bool compare(Particle &lParticle, Particle &hParticle) {
     return lParticle.getWeight() < hParticle.getWeight();
@@ -12,6 +16,16 @@ bool compare(Particle &lParticle, Particle &hParticle) {
 
 void uSort(std::vector<Particle>::iterator begin, std::vector<Particle>::iterator end) {
     std::sort(begin, end, compare);
+}
+
+Particle &getRandomParticle(MapLevel99Plus &map) {
+    std::random_device rd;
+    std::mt19937_64 eng(rd());
+    std::uniform_int_distribution<unsigned long long> distr;
+
+    long double randomNumber =  distr(eng) % PRIME;
+
+    return map[randomNumber];
 }
 
 #endif // UTILITIES_HPP
