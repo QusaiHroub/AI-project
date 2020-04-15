acf1d0ae252054a767c9cd4fa7802953d7ef50ce
farisabu3ram <aburamf@gmail.com>
2020-04-15 17:31:11 +0300

    print the values

--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,60 @@
 using namespace std;
 
 int main() {
-    return 0;
+	vector<Particle> v;//our particle
+	double arr[1000];
+	const double pi = 3.14159265;
+
+
+	int start = 6, currentPosition = start, step = 12;
+
+	double sum = 0;
+	int j = 0;
+	for (double i = 0; i < 1; i += 0.001) {
+		arr[j] = cos(2 * pi * i) + 0.5 * cos((3 * i + 0.23) * 2 * pi) + 0.5 * cos((5 * i - 0.4) * 2 * pi) + 0.5 * cos((7 * i + 2.09) * 2 * pi) + 0.5 * cos((9 * i - 3) * 2 * pi);
+		sum += arr[j];
+		j++;
+	}
+	double avg = sum / 1000;
+
+	sum = 0;
+	for (int i = 0; i < 1000; i++) {
+		sum += pow(arr[i] - avg, 2);
+
+	}
+
+	double standardDeviation = sum / 1000;
+
+	//to select particles
+	int pos = start;
+	double weightSum = 0;
+	double w = 0;
+	while (pos < 1000) {
+		Particle particle = new Particle();
+		particle.setPosition(pos);
+		double weightParticle = weight(arr[currentPosition] - arr[pos], avg, standardDeviation);
+		particle.setWeight(weightParticle);
+		weightSum += weightParticle;
+		v.push_back(particle);
+		pos = pos + step;
+	}
+	for (int i = 0; i < v.size(); i++)
+		v[i] = normalize(v[i], weightSum);
+
+	while (currentPosition < 1000) {
+		sum = 0;
+		for (int i = 0; i < v.size(); i++)
+		{
+			cout << "Particle number " << i << " in position : " << v[i].getPosition() << endl;
+			sum += v[i].getPosition();
+		}
+
+		cout << "Robot position : " << currentPosition << endl;
+		cout << "Mean : " << sum / v.size() << endl;
+		currentPosition += step;//need random error
+		v = Particle_filter(v, 10, arr[currentPosition], avg, standardDeviation, currentPosition, arr);//need random error arr[currentPosition]
+	}
+
+	return 0;
 }
+
--- src/particle_filter.hpp
+++ src/particle_filter.hpp
@@ -9,42 +9,44 @@
 
 const double pi = 3.14159265;
 
-/*
-To calculate the weights using gaussian formula **/
+/**
+To calculate the weights using gaussian formula */
 double weight(double x, double u, double q) {
 	return ((1 / (q * sqrt(2 * pi))) * exp(-1 * pow((x - u), 2) / 2 * pow(q, 2)));
 }
 /*
 To nornormalize the weights
 **/
-void normalize(std::vector<Particle>v,double w) {
-	for (int i = 0; i < v.size(); i++)
-		v[i].setWeight(v[i].getWeight() / w);
+Particle normalize(Particle p, double w) {
+	p.setWeight(p.getWeight() / w);
+	return p;
 }
 
 
-void Particle_filter(std::vector<Particle>v,int U, double Z,double avg,double standardDeviation,int currentPosition,double arr []) {
+std::vector<Particle> Particle_filter(std::vector<Particle>v, int U, double Z, double avg, double standardDeviation, int currentPosition, double arr[]) {
 	std::vector<Particle> newS;
 	MapLevel99Plus map;
 	map.buildForVector(v);
 	double weightSum = 0;
 	for (int i = 0; i < v.size(); i++) {
-        Particle randomParticle=Particle(&getRandomParticle(map));
-		if (randomParticle.getPosition() + U > 1000)
-			randomParticle.setPosition(rand()%(1000));
+		Particle randomParticle = Particle(&getRandomParticle(map));
+		if (randomParticle.getPosition() + U >= 1000)
+			randomParticle.setPosition(rand() % (1000));
 		else
-			randomParticle.setPosition(randomParticle.getPosition() + U) ;
-		double weightParicle= weight(Z - arr[randomParticle.getPosition()], avg, standardDeviation);
+			randomParticle.setPosition(randomParticle.getPosition() + U);
+		double weightParicle = weight(Z - arr[randomParticle.getPosition()], avg, standardDeviation);
 		randomParticle.setWeight(weightParicle);
 		weightSum += weightParicle;
 		newS.push_back(randomParticle);
 	};
-	for (int i = 0; i < v.size(); i++){
-		v[i]=newS[i];
+	for (int i = 0; i < v.size(); i++) {
+		v[i] = newS[i];
 	}
-	normalize(v,weightSum);
+	for (int i = 0; i < v.size(); i++)
+		v[i] = normalize(v[i], weightSum);
 
 
+	return v;
 }
 
 #endif // PARTICLE_FILTER_HPP_INCLUDED
