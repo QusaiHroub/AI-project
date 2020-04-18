4d24d95f7304d2e744a50dcf9594d0a15e20476b
QusaiHroub <qusaihroub.r@gmail.com>
2020-04-18 16:59:08 +0300

    set default valuse

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <string>
+#include <sstream>
 
-#include "maplevel99plus.hpp"
 #include "particle.hpp"
 #include "particle_filter.hpp"
 #include "utilities.hpp"
@@ -10,7 +10,13 @@
 
 using namespace std;
 
+int readInt(int &);
+
 int main() {
+
+	const int DEFAULT_STEP_LENGTH = 11;
+	const int DEFAULT_START_POSITION = 13;
+	const int DEFAULT_PARTICLES_NUMBER = 45;
 
 	//list contain the particles of robot
 	vector<Particle> particlesList;
@@ -24,24 +30,34 @@ int main() {
 
 	//heat sensor (: to get the temperature
 	HeatSensor heatSensor(arr, 1000);
-
-	int start , stepSize, nextStep;
-	cout << "Enter the start point of rebot : ";
-	cin >> start;
-	cout << "Enter the step length  of rebot : ";
-	cin >> stepSize;
+	
+	int start = DEFAULT_START_POSITION, stepSize = DEFAULT_STEP_LENGTH, nextStep;
+	
+	cout << "Enter the start point of rebot (default = " << DEFAULT_START_POSITION << ") : ";
+	while (readInt(start)) {
+		cout << "Invalid integer. Try again (default = " << DEFAULT_START_POSITION << ") : ";
+	}
+	
+	cout << "Enter the step length  of rebot (default = " << DEFAULT_STEP_LENGTH << ") : ";
+	while (readInt(stepSize)) {
+		cout << "Invalid integer. Try again (default = " << DEFAULT_STEP_LENGTH << ") : ";
+	}
 
 	int  currentPosition = start;
 
 	double standardDeviation = utilities::getStandardDeviation(arr, avg);
-	double numberOfRandomParticles;
-	cout << "Enter the number of particles : ";
-	cin >> numberOfRandomParticles;
+	int numberOfRandomParticles = DEFAULT_PARTICLES_NUMBER;
+	
+	cout << "Enter the number of particles (default = " << DEFAULT_PARTICLES_NUMBER << ") : ";
+	while (readInt(numberOfRandomParticles)) {
+		cout << "Invalid integer. Try again (default = " << DEFAULT_PARTICLES_NUMBER << ") : ";
+	}
 
 	//function to  select random particles
 	utilities::getRandomParticles(particlesList, arr, currentPosition, avg, standardDeviation, numberOfRandomParticles);
 	double sum;
-	while (currentPosition < 1000) {
+	
+	while (cout << "press enter to continue ... ", currentPosition < 1000 && cin.get() == '\n') {
 		sum = 0;
 
 		//print position for all particles.
@@ -66,5 +82,15 @@ int main() {
 		currentPosition += nextStep;
 	}
 
+	return 0;
+}
+
+int readInt(int &inputRef) {
+	string input;
+
+	getline(cin, input);
+	if (!input.empty() && !(stringstream(input) >> inputRef)) {
+		return -1;
+	} 
 	return 0;
 }
